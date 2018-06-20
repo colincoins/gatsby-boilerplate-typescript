22296f32adc051ba59a333a45e4fb591aecccddb
Colin Chen <chencolin.me@gmail.com>
2018-06-20 13:21:17 -0700

    Added Gatsby Plugin React CSS Modules * Gatsby Plugin PostCSS Sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,27 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Default Starter',
   },
-  plugins: ['gatsby-plugin-typescript', 'gatsby-plugin-sass']
+  plugins: ['gatsby-plugin-typescript',
+  {
+    resolve: 'gatsby-plugin-react-css-modules',
+    options: {
+      // *.css files are included by default.
+      // To support another syntax (e.g. SCSS),
+      // add `postcss-scss` to your project's devDependencies
+      // and add the following option here:
+      filetypes: {
+        '.scss': { syntax: 'postcss-scss' }
+      },
+
+      // Exclude global styles from the plugin using a RegExp:
+      exclude: '\/global\/'
+    }
+  },
+  {
+    resolve: 'gatsby-plugin-postcss-sass',
+    options: {
+      postCssPlugins: [],
+      precision: 8 // SASS default: 5
+    }
+  }]
 }
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react'
-// import styles from './header.module.scss'
+import styles from './header.module.scss'
 
 class Header extends React.PureComponent {
   render() {
     return (
-      // <div className={styles['header']}>
-      <div>
+      <div className={styles['header']}>
+      {/* <div> */}
       {/* <div>   */}
         This is the heade!!r
       </div>
